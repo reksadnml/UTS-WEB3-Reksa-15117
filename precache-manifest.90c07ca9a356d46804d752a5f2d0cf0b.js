9e2c2eeed43c4529ed8612e1c7e2cd7d846fa89e
Reksa Milenia <reksa.milenia@gmail.com>
2020-04-15 15:57:24 +0700

    Updates

---
+++ precache-manifest.90c07ca9a356d46804d752a5f2d0cf0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5eacc918d319e275c2bcce6dad164b",
+    "url": "/UTS-WEB3-Reksa-15117/index.html"
+  },
+  {
+    "revision": "2e7af3571be4894b5a39",
+    "url": "/UTS-WEB3-Reksa-15117/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1e29a0acf61080dd71f4",
+    "url": "/UTS-WEB3-Reksa-15117/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e7af3571be4894b5a39",
+    "url": "/UTS-WEB3-Reksa-15117/static/js/2.c39e3bc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UTS-WEB3-Reksa-15117/static/js/2.c39e3bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e29a0acf61080dd71f4",
+    "url": "/UTS-WEB3-Reksa-15117/static/js/main.3c480b3c.chunk.js"
+  },
+  {
+    "revision": "83b49c39df34e48ad7e7",
+    "url": "/UTS-WEB3-Reksa-15117/static/js/runtime-main.2412c34c.js"
+  }
+]);
